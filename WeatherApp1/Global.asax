a133dccfc8c981b2e1e5f0ddb3fca857e0678927
csirirathasuk14 <csirirathasuk14@gmail.com>
2016-10-26 19:34:35 -0400

    first commit

---
+++ WeatherApp1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeatherApp1.MvcApplication" Language="C#" %>
---
+++ WeatherApp1/Models/Weather.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp1.Models
+{
+    public class Weather
+    {
+        public Object getWeatherForcast()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=ac5023b431549318bd42a0683808c501&units=imperial";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+    }
+}
